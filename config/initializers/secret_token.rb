f79cb8cd5159cfcce0b02d452355f508671ef270
Lorgio Antonio Jimenez <lorgio.a.jimenez@aexp.com>
2011-06-14 13:04:05 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedList::Application.config.secret_token = '84326bf7d18770d0615c77d1ef7b8368698c683a058ae5e876e6a4ebe46b54cee0594452e5017bc2427ffa2ab1e75afd870c1a46a2cb1b154f2455078fb17f6e'
